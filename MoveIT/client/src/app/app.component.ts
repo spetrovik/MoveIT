3701d04925583bb325289fc07fed07e2e1b1b611
Sanja <spetrovik3@gmail.com>
2022-03-28 23:47:08 +0200

    Login/Register

--- MoveIT/client/src/app/app.component.ts
+++ MoveIT/client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
+
 
 @Component({
   selector: 'app-root',
@@ -10,12 +13,16 @@ export class AppComponent implements OnInit {
   title = 'Move IT company';
   users:any;
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private accountService: AccountService){}
   ngOnInit(){
   this.getUsers();
+  this.setCurrentUser();
   }
-
-  getUsers(){
+  setCurrentUser(){
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
+  getUsers(): void{
     this.http.get('https://localhost:5001/api/users')
     .subscribe(response => {
       this.users = response;
--- MoveIT/client/src/app/app.module.ts
+++ MoveIT/client/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    HttpClientModule
+    HttpClientModule, 
+    BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
